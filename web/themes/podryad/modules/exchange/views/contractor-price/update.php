fce43fbafe7c4f08955f33305466cb0ee6056c51
ivphpan <vodyanov.ivan@yandex.ru>
2015-10-21 10:43:51 +0500

    убрал пугающие надписи

--- web/themes/podryad/modules/exchange/views/contractor-price/update.php
+++ web/themes/podryad/modules/exchange/views/contractor-price/update.php
@@ -2,10 +2,4 @@
 /* @var $this yii\web\View */
 /* @var $model \app\modules\exchange\models\ContractorPrice */
 ?>
-<h1>contractor-price/update</h1>
-
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
 <?= $this->render('_form', ['model' => $model]) ?>
