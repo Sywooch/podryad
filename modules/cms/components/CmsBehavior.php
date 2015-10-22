e952b94e9509f491f7e646c5d129c9d34ad0fb13
ivphpan <vodyanov.ivan@yandex.ru>
2015-10-22 17:05:30 +0500

    image fix

--- modules/cms/components/CmsBehavior.php
+++ modules/cms/components/CmsBehavior.php
@@ -20,11 +20,11 @@ class CmsBehavior extends Behavior{
 
     public function imageSrc($size='100x100',$method = Thumbler::METHOD_NOT_BOXED)
     {
-        /*if(!method_exists($this->owner,'getImage'))
-            return $this->imageThumb.$size;*/
+        if(!method_exists($this->owner,'getImage'))
+            return $this->imageThumb.$size;
 
         $image = $this->owner->image;
-        if($image && !is_file(\Yii::$app->thumbler->sourcePath.'/'.$image->src))
+        if($image && !is_file(\Yii::$app->thumbler->sourcePath.$image->src))
         {
             return $this->imageThumb . $size;
         }
