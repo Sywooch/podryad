4b89127c8c7075da0a2de446653a22c81c6a166a
Nurzhik <nurzhanbalkibek@gmail.com>
2015-10-23 21:55:15 +0600

    Измееил кнопку

--- web/themes/podryad/modules/exchange/widgets/views/reviews.php
+++ web/themes/podryad/modules/exchange/widgets/views/reviews.php
@@ -45,7 +45,7 @@
         <?=\yii\helpers\Html::activeHiddenInput($model,'contractorId')?>
         <?=$form->field($model,'rate')->radioList(['Дислайк','Лайк'])?>
         <?=$form->field($model,'content')->textarea()?>
-        <?=\yii\helpers\Html::submitButton('Добавить'),['class'=>'create-revie btn']?>
+        <?=\yii\helpers\Html::submitButton('Добавить',['class'=>'create-revie btn'])?>
         <?php \yii\widgets\ActiveForm::end()?>
     <?php endif?>
 </div>
