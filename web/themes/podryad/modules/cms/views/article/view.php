2e2e21fe1b293f97d3ef42f59202b2a77223ee39
ivphpan <vodyanov.ivan@yandex.ru>
2015-10-23 15:50:43 +0500

    добавил hypercomments в новости

--- web/themes/podryad/modules/cms/views/article/view.php
+++ web/themes/podryad/modules/cms/views/article/view.php
@@ -14,7 +14,27 @@ $this->title = $item->title;
         <?=$item->description?>
         <div class="news-full-content__social">Поделиться в социальных сетях<img src="<?= $this->theme->getUrl('static/images/content/social.jpg') ?>"
                                                                                  alt=""></div>
-        <div class="news-full-content__comments"><img src="<?=$this->theme->getUrl('static/images/content/comments.jpg')?>" alt=""></div>
+        <div class="news-full-content__comments">
+            <div id="hypercomments_widget"></div>
+            <script type="text/javascript">
+                _hcwp = window._hcwp || [];
+                _hcwp.push({widget: "Stream", widget_id: 65620});
+                (function () {
+                    if ("HC_LOAD_INIT" in window)return;
+                    HC_LOAD_INIT = true;
+                    var lang = (navigator.language || navigator.systemLanguage || navigator.userLanguage || "en").substr(0, 2).toLowerCase();
+                    var hcc = document.createElement("script");
+                    hcc.type = "text/javascript";
+                    hcc.async = true;
+                    hcc.src = ("https:" == document.location.protocol ? "https" : "http") + "://w.hypercomments.com/widget/hc/65620/" + lang + "/widget.js";
+                    var s = document.getElementsByTagName("script")[0];
+                    s.parentNode.insertBefore(hcc, s.nextSibling);
+                })();
+            </script>
+            <a href="http://hypercomments.com" class="hc-link" title="comments widget">comments powered by
+                HyperComments
+            </a>
+        </div>
     </section>
     <?=$this->render('//layouts/_sidebar')?>
 </main>
