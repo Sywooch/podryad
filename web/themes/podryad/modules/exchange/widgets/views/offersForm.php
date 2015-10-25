2e0816469156be863700a7796c85e531b6ea3934
Nurzhik <nurzhanbalkibek@gmail.com>
2015-10-25 23:02:04 +0600

    Вернул удаленные файлы 2

---
+++ web/themes/podryad/modules/exchange/widgets/views/offersForm.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivphpan
+ * Date: 03.10.15
+ * Time: 11:43
+ * @var $model \app\modules\exchange\models\Offers
+ * @var $tenderId integer
+ */
+use yii\widgets\ActiveForm;
+?>
+<div id="zakaz_tender" class="modal zakaz_tender">
+    <div class="modal__close">x</div>
+    <div class="modal__title">Участвовать в тендере</div>
+        <?php $form = ActiveForm::begin([
+            'id'=>'offers-form',
+            'options'=>['class'=>'form'],
+            'validationUrl'=>['/exchange/offers/validate-create'],
+            'action'=>['/exchange/offers/create','tenderId'=>$tenderId],
+            'fieldConfig'=>[
+                'labelOptions'=>['form__label form__label--required'],
+            ],
+            'enableAjaxValidation'=>true,
+        ])?>
+        <?= $form->field($model, 'price')->textInput(['class'=>'form__input']) ?>
+        <?= $form->field($model, 'description')->textarea(['class' => 'form_textarea']) ?>
+        <input type="submit" value="Участвовать" class="form__submit">
+    <?php ActiveForm::end()?>
+</div>
