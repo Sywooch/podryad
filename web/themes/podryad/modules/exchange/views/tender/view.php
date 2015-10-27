24624fc00ac2ec455765845c6202701c38914ca3
ivphpan <vodyanov.ivan@yandex.ru>
2015-10-27 14:33:09 +0500

    min fix

--- web/themes/podryad/modules/exchange/views/tender/view.php
+++ web/themes/podryad/modules/exchange/views/tender/view.php
@@ -20,7 +20,7 @@ $offersCount = sizeof($offers);
 ?>
 <main class="main">
     <div class="tender_cotainer">
-        <a href="<?=\Yii::$app->request->referrer?>" title="" class="contractor-back">Вернуться к списку тендеров</a>
+        <a href="<?=\yii\helpers\Url::to(['/exchange/tender'])?>" title="" class="contractor-back">Вернуться к списку тендеров</a>
         <div class="tender_item__cotainer">
             <div class="tender_item_des">
                 <h1 class="tender_item_des_title"><?=$model->title?></h1>
