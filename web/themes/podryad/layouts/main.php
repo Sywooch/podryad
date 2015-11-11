6567868113f955ae86135eba4fa9ef0eee3d0ecb
Nurzhik <nurzhanbalkibek@gmail.com>
2015-11-11 13:11:31 +0600

    фавикон

--- web/themes/podryad/layouts/main.php
+++ web/themes/podryad/layouts/main.php
@@ -44,7 +44,7 @@ $this->beginPage();
 				<link rel="icon" type="image/png" sizes="32x32" href="themes/podryad/static/images/favicon/favicon-32x32.png">
 				<link rel="icon" type="image/png" sizes="96x96" href="themes/podryad/static/images/favicon/favicon-96x96.png">
 				<link rel="icon" type="image/png" sizes="16x16" href="themes/podryad/static/images/favicon/favicon-16x16.png">
-				<link rel="manifest" href="static/images/favicon/manifest.json">
+				<link rel="manifest" href="themes/podryad/static/images/favicon/manifest.json">
 				<meta name="msapplication-TileColor" content="#ffffff">
 				<meta name="msapplication-TileImage" content="themes/podryad/static/images/favicon/ms-icon-144x144.png">
 				<meta name="theme-color" content="#ffffff">
