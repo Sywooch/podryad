ed9fc64a563132439cde6b7d2d00c888bfd042a0
Nurzhik <nurzhanbalkibek@gmail.com>
2015-12-25 01:36:51 +0600

    предложение

--- mail/offers/create.php
+++ mail/offers/create.php
@@ -10,22 +10,41 @@
  */
 $link = \yii\helpers\Url::to(['/exchange/tender/view', 'id' => $model->tender->id], true);
 ?>
-<h1><?=$subject?></h1>
-<div>
-    <ul>
-        <li>Имя: <?=$model->tender->user->title?></li>
-        <li>Телефон: <?=$model->tender->user->profile->phone?></li>
-        <li>
-            Тендер:
-            <a href="<?=$link?>"><?=$model->tender->title?></a>
-        </li>
-        <li>Цена: <?=$model->price?></li>
-        <li>Описание: <?=$model->description?></li>
-    </ul>
-</div>
-<p>
-    Для подробного проссмотра предложения, вам нужно перейти по
-    <a href="<?=$link?>">
-    ссылке.
-    </a>
-</p>
+<table width="616px" cellpadding="0" cellspacing="0" style="border-collapse:collapse; border:none;margin:0 auto;" 
+
+border="0">
+<tbody>
+<tr>
+	<td width="100%" height="10" style="text-align:center;">
+		<a href="http://podryad.kz/"target="_blank"><img 
+
+src="http://podryad.kz/themes/podryad/static/images/logo_ras.jpg" alt="podryad.kz" width="95px" height="114px">
+	</td>
+</tr>
+<tr>
+	<td style=" padding: 20px; ">
+		<p style=" color:#3f3f3f;font-family:Tahoma;font-size:14px; margin-bottom:20px ">
+			Здравствуйте, <?=$model->tender->user->title?>!
+		</p>
+		<p style=" color:#3f3f3f;font-size:14px; margin:0;font-family:Tahoma; margin-bottom:8px ">
+			Вам поступило предложение на выполнение объёма работ, описанного  
+		</p>
+		<p style=" color:#3f3f3f;font-size:14px; margin:0;font-family:Tahoma; margin-bottom:8px ">
+			в Вашем тендере   <a href="<?=$link?>"><?=$model->tender->title?></a>.
+		</p>
+                <p style=" color:#3f3f3f;font-size:14px; margin:0; font-family:Tahoma; margin-bottom:8px">
+			Для подробного просмотра предложения, пожалуйста, пройдите по ссылке 
+		</p>
+                <p style=" color:#3f3f3f;font-size:14px; margin:0; font-family:Tahoma; margin-bottom:20px">
+			на страничку тендера. 
+		</p>
+		<p style=" color:#3f3f3f;font-size:14px; margin:0; font-family:Tahoma; margin-bottom:10px">
+			С уважением,
+		</p>
+		<p style=" color:#3f3f3f;font-size:14px; margin:0;font-family:Tahoma; margin-bottom:20px ">
+			команда podryad.kz
+		</p>
+	</td>
+</tr>
+</tbody>
+</table>
