b9903eb2304cdecb7fc31e171314dc947a40c9c5
Nurzhik <nurzhanbalkibek@gmail.com>
2015-12-25 01:40:40 +0600

    принять

--- mail/contractor/notify.php
+++ mail/contractor/notify.php
@@ -10,8 +10,38 @@
 
 use yii\helpers\Url;
 ?>
-<h1><?=$subject?></h1>
-<p>
-    Вас пригласили для участия в тендере
-     "<a href="<?=Url::to(['/exchange/tender/view','id'=>$tender->id],true)?>"><?=$tender->title?></a>"
-</p>
+<table width="616px" cellpadding="0" cellspacing="0" style="border-collapse:collapse; border:none;margin:0 auto;" 
+
+border="0">
+<tbody>
+<tr>
+	<td width="100%" height="10" style="text-align:center;">
+		<a href="http://podryad.kz/"target="_blank"><img 
+
+src="http://podryad.kz/themes/podryad/static/images/logo_ras.jpg" alt="podryad.kz" width="95px" height="114px">
+	</td>
+</tr>
+<tr>
+	<td style=" padding: 20px; ">
+		<p style=" color:#3f3f3f;font-family:Tahoma;font-size:14px; margin-bottom:20px ">
+			Здравствуйте!
+		</p>
+		<p style=" color:#3f3f3f;font-size:14px; margin:0;font-family:Tahoma; margin-bottom:8px ">
+			Вас приглашают принять участие в тендере:
+		</p>
+		<p style=" color:#3f3f3f;font-size:14px; margin:0;font-family:Tahoma; margin-bottom:8px ">
+			<a href="<?=Url::to(['/exchange/tender/view','id'=>$tender->id],true)?>"><?=$tender->title?></a>
+		</p>
+                <p style=" color:#3f3f3f;font-size:14px; margin:0; font-family:Tahoma; margin-bottom:20px">
+			Пройдите по ссылке и оставьте своё предложение, если заказ Вам интересен.
+		</p>
+		<p style=" color:#3f3f3f;font-size:14px; margin:0; font-family:Tahoma; margin-bottom:10px">
+			С уважением,
+		</p>
+		<p style=" color:#3f3f3f;font-size:14px; margin:0;font-family:Tahoma; margin-bottom:20px ">
+			команда podryad.kz
+		</p>
+	</td>
+</tr>
+</tbody>
+</table>
