dc6d0d98c5d7c1ca855481ba2d3c5092ba5ed01d
ivphpan <vodyanov.ivan@yandex.ru>
2016-08-25 10:29:00 +0500

    изменил поведение навигации

--- web/themes/podryad/modules/exchange/widgets/views/navigation.php
+++ web/themes/podryad/modules/exchange/widgets/views/navigation.php
@@ -7,10 +7,10 @@
 <div class="filter">
     <?= \app\modules\cms\widgets\City::widget() ?>
     <?php foreach($root->children() as $item):?>
-    <div class="filter__item active">
+    <div class="filter__item <?=$item->id == $model->parent->id ? 'active' : ''?>">
         <a class="filter__title" href="<?=$item->url?>"><?=$item->title?></a>
         <?php if (($childrens = $item->children())): ?>
-        <div class="filter__list active">
+        <div class="filter__list <?= $item->id == $model->parent->id ? 'active' : '' ?>">
             <?php foreach($childrens as $children):?>
             <a class="filter-item <?=$children->id == $model->id ? 'active' : ''?>" href="<?=$children->url?>"><?=$children->title?></a>
             <?php endforeach?>
